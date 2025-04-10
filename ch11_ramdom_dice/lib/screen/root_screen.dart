fafd0fef257dac1ea2cc32f9b98a448bb2d7579f
XII1071 <garmen1071@gmail.com>
2025-04-10 11:47:41 +0900

    0410

--- ch11_ramdom_dice/lib/screen/root_screen.dart
+++ ch11_ramdom_dice/lib/screen/root_screen.dart
@@ -1,7 +1,6 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class RootScreen extends StatelessWidget {
+class RootScreen extends StatefulWidget {
   const RootScreen({Key? key}) : super(key: key);
 
   @override
@@ -15,21 +14,58 @@ class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
   void initState() {
     super.initState();
     controller = TabController(length: 2, vsync: this);
+    controller!.addListener(tabListener);
+  }
+
+  tabListener() {
+    setState(() {});
+  }
+
+  @override
+  void dispose() {
+    // 이 클래스가 소멸될 때 tabListener를 소멸시켜 메모리 누수 방지
+    controller!.removeListener(tabListener);
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TabBarView(children: renderChildren()),
+      body: TabBarView(controller: controller, children: renderChildren()),
       bottomNavigationBar: renderBottomNavigation(),
     );
   }
 
   List<Widget> renderChildren() {
-    return [];
+    return [
+      Container(
+        child: Center(
+          child: Text('Tab 1', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+      Container(
+        child: Center(
+          child: Text('Tab 2', style: TextStyle(color: Colors.white)),
+        ),
+      ),
+    ];
   }
 
   BottomNavigationBar renderBottomNavigation() {
-    return BottomNavigationBar(items: []);
+    return BottomNavigationBar(
+      currentIndex: controller!.index,
+      onTap: (int index) {
+        setState(() {
+          controller!.animateTo(index);
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.edgesensor_high_outlined),
+          label: "주사위",
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), label: "설정"),
+      ],
+    );
   }
 }
