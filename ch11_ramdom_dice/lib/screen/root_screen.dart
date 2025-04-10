e836ac052b144f80c04d122f47f91f6e8979f319
XII1071 <garmen1071@gmail.com>
2025-04-10 11:13:32 +0900

    0410

---
+++ ch11_ramdom_dice/lib/screen/root_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class RootScreen extends StatelessWidget {
+  const RootScreen({Key? key}) : super(key: key);
+
+  @override
+  State<RootScreen> createState() => _RootScreenState();
+}
+
+class _RootScreenState extends State<RootScreen> with TickerProviderStateMixin {
+  TabController? controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: TabBarView(children: renderChildren()),
+      bottomNavigationBar: renderBottomNavigation(),
+    );
+  }
+
+  List<Widget> renderChildren() {
+    return [];
+  }
+
+  BottomNavigationBar renderBottomNavigation() {
+    return BottomNavigationBar(items: []);
+  }
+}
