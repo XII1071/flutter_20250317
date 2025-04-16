2960c07780d976855e1e672fa655a9ad8f08ad88
XII1071 <garmen1071@gmail.com>
2025-04-16 10:50:09 +0900

    0416

---
+++ ch19_scheduler_api/android/app/src/main/kotlin/com/example/ch19_scheduler_api/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ch19_scheduler_api
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ ch19_scheduler_api/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:ch19_scheduler_api/database/drift_database.dart';
+import 'package:ch19_scheduler_api/screen/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:intl/date_symbol_data_local.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await initializeDateFormatting('ko_KR');
+
+  final database = LocalDatabase(); // 데이터베이스 생성
+
+  GetIt.I.registerSingleton<LocalDatabase>(database);
+
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()));
+}
