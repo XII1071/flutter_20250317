90de4d171caa60261063f073c2db565943295c2d
XII1071 <garmen1071@gmail.com>
2025-04-18 10:54:53 +0900

    0418

--- ch21_scheduler_token/lib/screen/home_screen.dart
+++ ch21_scheduler_token/lib/screen/home_screen.dart
@@ -47,6 +47,7 @@ class HomeScreen extends StatelessWidget {
         child: Column(
           // 달력과 리스트를 세로로 배치
           children: [
+            Text("ch21_scheduler_token"),
             MainCalendar(
               selectedDate: selectedDate, // 선택된 날짜 전달하기
               // 날짜가 선택됐을 때 실행할 함수
